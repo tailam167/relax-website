86e039a6b018f91641a96a80a58bb895dddc4932
AAVN\lttai01 <lttai01@axonactive.com>
2024-10-29 17:30:18 +0700

    second commit

--- config.js
+++ config.js
@@ -1,14 +1,11 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Xin mời đồng chí A Huiii lên !`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <3',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Kài mụp mãi đỉnh <3',
+    btnNo: 'Không, Kài mụp à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <3',
